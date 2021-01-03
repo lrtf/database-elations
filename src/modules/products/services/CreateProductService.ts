6bfb23814677b5ddcfc04f2ccc9c02ae11ad50d3
Luis Ricardo Teixeira Fernandes <lrtf.developer@gmail.com>
2021-01-03 18:39:30 -0300

    Desafio finalizado

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -1,7 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-
 import Product from '../infra/typeorm/entities/Product';
 import IProductsRepository from '../repositories/IProductsRepository';
 
@@ -13,10 +12,25 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const productsExists = await this.productsRepository.findByName(name);
+
+    if (productsExists) {
+      throw new AppError('There is already one product with thos name');
+    }
+
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return product;
   }
 }
 
